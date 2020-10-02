179577dd0ee2057b3ec0526eb6687987d6133dd0
JovanaT99 <66609488+JovanaT99@users.noreply.github.com>
2020-10-02 16:36:48 +0200

    Update Duz.java

--- Duz.java
+++ Duz.java
@@ -12,6 +12,12 @@ public Tacka getPocetak(){
 public Tacka getKraj() {
 	 	return this.kraj;
  		}
+public void setX(int x) {
+		this.x = x;
+		}
+public void setY(int y) {
+		this.y = y;
+		}
 public double duzina(){
 	 	return Math.sqrt(Math.pow((this.kraj.getX()-this.pocetak.getX()),2) + Math.pow(this.kraj.getY()-this.pocetak.getY(),2));
 		}
