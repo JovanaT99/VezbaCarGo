eaf0e0ee90faef26b569ee68c0fd850cc1423177
JovanaT99 <66609488+JovanaT99@users.noreply.github.com>
2020-10-02 16:29:42 +0200

    Update TestGeometrija.java

--- TestGeometrija.java
+++ TestGeometrija.java
@@ -1,9 +1,7 @@
 package juniorjava;
 
 public class TestGeometrija {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Tacka t1 = new Tacka(4, 5);
 		Tacka t2 = new Tacka();
 
@@ -14,8 +12,7 @@ public class TestGeometrija {
 		System.out.println("Tacka t1 posle pomeraja: " + t1.toString());
 		
 		Duz duz = new Duz(t1, t2);
-		System.out.println("Duz: " +duz.toString());
+		System.out.println("Duz: " + duz.toString());
 		System.out.print("Duzina duzi je: " + duz.duzina());
 	}
-
 }
