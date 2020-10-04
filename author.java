565f6b09bf3df7f0128a360176c327530fe7defb
JovanaT99 <jovanablagojevic98@gmail.com>
2020-10-04 23:12:14 +0200

    Homework

---
+++ author.java
@@ -0,0 +1,53 @@
+/*Zadatak 2: Napisati dve klase Author( name, email, gender)i
+ *Book (name, author, price, quantity)i
+ *trecu test klasu.
+ */
+package Author;
+
+public class Author {
+
+	private String name;
+	private String email;
+	private String gender;
+	
+	public Author(String name,String email,String gender) {
+		this.name=name;
+		this.email=email;
+		this.gender=gender;
+		
+	}
+	//get
+	public String getName() {
+		
+		return this.name;
+		
+	}
+	public String getEmail() {
+		return this.email;
+		
+	}
+	public String getGender() {
+		return this.gender;
+		
+	}
+	//set
+	public void setName(String name)
+	{
+		this.name=name;
+		
+	}
+	public void setEmail(String email)
+	{
+		this.email=email;
+		
+	}
+	public void setGender(String gender)
+	{
+		this.gender=gender;
+		
+	}
+	public String toString() {
+		return "(" + name.toString() + ", " + email.toString()+ "," +gender.toString()+")";
+		
+	}
+}
---
+++ authortest.java
@@ -0,0 +1,11 @@
+package Author;
+
+public class authortest {
+
+	public static void main(String[]args) {
+		
+		Author Autor=new Author("Test ime","testuser@gmail.com","musko");
+	
+		System.out.println(Autor.toString());
+	}
+}
