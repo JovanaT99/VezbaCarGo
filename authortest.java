d08e02bbff5159baf45ba919b632f760248e92c3
JovanaT99 <66609488+JovanaT99@users.noreply.github.com>
2020-10-04 23:15:03 +0200

    Update authortest.java

--- authortest.java
+++ authortest.java
@@ -1,11 +1,10 @@
+// main Author
 package Author;
-
 public class authortest {
 
 	public static void main(String[]args) {
 		
 		Author Autor=new Author("Test ime","testuser@gmail.com","musko");
-	
 		System.out.println(Autor.toString());
 	}
 }
