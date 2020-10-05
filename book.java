779e813a580b865cfa4873880ac8a67db7c74fe2
JovanaT99 <66609488+JovanaT99@users.noreply.github.com>
2020-10-05 10:27:06 +0200

    Add files via upload

---
+++ book.java
@@ -0,0 +1,38 @@
+package Author;
+
+public class Book {
+
+		private String name;
+		private Author author;
+		private double price;
+		
+		Book (String name, Author author, int price) {
+			this.name = name;
+			this.author = author;
+			this.price = price;
+		}
+		//get 
+		public String getName() {
+			return this.name;
+		}
+		public Author getAuthor() {
+			return this.author;
+		}
+		public int getPrice() {
+			return (int) this.price;
+		}
+		//set 
+		public void setName(String name) {
+			this.name = name;	
+		}
+		public void setAuthor(Author author) {
+			this.author = author;
+		}
+		public void setPrice(int price) {
+			this.price = price;
+			
+		}
+		public String toString() {
+			return return "(" + name.getName() + ", " + author.toString()+ "," +price.getPrice()+")";
+		}
+}
