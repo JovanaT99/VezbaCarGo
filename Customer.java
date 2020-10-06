7bb2b29bb6d48c7c8ad5367612f3e36fc56aee6f
JovanaT99 <jovanablagojevic98@gmail.com>
2020-10-06 16:18:24 +0200

    maven

---
+++ Customer.java
@@ -0,0 +1,29 @@
+public class Customer {
+
+    private String name;
+    private String email;
+    private int age;
+    //get
+
+    public String getName() {
+        return this.name;
+    }
+    public String getEmail() {
+        return this.email;
+    }
+    public int getAge() {
+        return this.age;
+    }
+
+    //set
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ MainClass.java
@@ -0,0 +1,18 @@
+import org.yaml.snakeyaml.Yaml;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainClass {
+
+    public static void main(String[] args) throws IOException {
+        Yaml yaml = new Yaml();
+        try (InputStream in = MainClass.class
+                .getResourceAsStream("/Customer.yml")) {
+            Customer customer= yaml.loadAs(in, Customer.class);
+            System.out.println(customer.getEmail());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
