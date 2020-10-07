eb9e26eb65d6b6b0841a10a7285388a347492e17
JovanaT99 <66609488+JovanaT99@users.noreply.github.com>
2020-10-07 17:19:39 +0200

    Update TestHashMap.java

--- TestHashMap.java
+++ TestHashMap.java
@@ -1,12 +1,15 @@
 import java.util.HashMap;
 public class TestHashMap {
-
     public static void main(String[]args) {
         HashMap<String,Integer> map = new HashMap<>();
         
         map.put("java", 1);
         map.put("test", 2);
         map.put("Tifani",3);
+        //get
+        //remove
+        //clear
+        //size
         System.out.println(map.size());
     }
 }
