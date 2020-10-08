2983fe330fed948a3c5911e1b834762e51453e9a
JovanaT99 <jovanablagojevic98@gmail.com>
2020-10-08 13:23:31 +0200

    Liste

---
+++ Persons.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Persons {
+
+  private List<Person> persons;
+
+  public List<Person> getPersons() {
+    return persons;
+  }
+  public void setPersons(List<Person> persons) {
+    this.persons= persons;
+  }
+}
