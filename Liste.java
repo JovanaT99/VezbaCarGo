bacfcb96de79a2feb936ef57500f4acae1f5075f
JovanaT99 <66609488+JovanaT99@users.noreply.github.com>
2020-10-09 13:46:37 +0200

    Update Liste.java

--- Liste.java
+++ Liste.java
@@ -7,12 +7,9 @@ public class Liste {
         Yaml yaml = new Yaml();
         try (InputStream in = Liste.class.getResourceAsStream("/persons.yaml")) {
             Persons persons = yaml.loadAs(in, Persons.class);
-
-            // Person tifani = persons.getPersons().get(0);
             for (Person person : persons.getPersons()) {
                 System.out.println(person.getCar().getType());
             }
-            //System.out.println(tifani.getAddress());
         }
     }
 }
