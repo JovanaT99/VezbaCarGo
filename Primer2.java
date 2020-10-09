98b3030752c6ed3080c18ffd1645e1ad8c57b2f3
JovanaT99 <jovanablagojevic98@gmail.com>
2020-10-09 13:44:00 +0200

    Zadatak 2

---
+++ Primer2.java
@@ -0,0 +1,21 @@
+import org.yaml.snakeyaml.Yaml;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Primer2 {
+    public static void main(String[] args) throws IOException {
+        //loadFromFile("/person.yml");
+        loadFromFile();
+    }
+    private static void loadFromFile() throws IOException{
+        System.out.println("/primer2.yaml");
+        Yaml yaml = new Yaml();
+        try(InputStream in = Primer2.class.getResourceAsStream("/primer2.yaml")){
+            Iterable<Object> itr =yaml.loadAll(in);
+            for(Object j: itr){
+                System.out.println("test" + j.getClass());
+                System.out.println(j);
+            }
+        }
+    }
+}
