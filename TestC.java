73a87372b3b62be4c0d2d82e797c9450e3269d44
JovanaT99 <66609488+JovanaT99@users.noreply.github.com>
2020-10-12 17:16:15 +0200

    Update TestC.java

--- TestC.java
+++ TestC.java
@@ -1,7 +1,13 @@
 public class TestC {
-
     public static void main(String[] args) {
-        Pas pas = new Pas("Dzeki");
+        Pas pas = new Pas("Dzeki","Vucijak");
+        Pas pas2 = new Pas("Jack");
+        Zivotinja tif = new Zivotinja("Borcanski slatki psic", "Mik");
+        Zivotinja dzonika = new Pas("Dzesi");
         System.out.println( pas.getIme()+ ',' + pas.getRasa());
+        System.out.println( pas2.getIme()+ ',' + pas2.getRasa());
+        System.out.println(tif.getRasa());
+        System.out.println(dzonika.getRasa());
+        
     }
 }
