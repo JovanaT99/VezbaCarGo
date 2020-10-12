dd9e3f065380b3af19424fa00831cfa234a2df51
JovanaT99 <66609488+JovanaT99@users.noreply.github.com>
2020-10-12 17:14:03 +0200

    Update Tea2.java

--- Tea2.java
+++ Tea2.java
@@ -9,6 +9,7 @@ public class Tea2 {
 
     public Tea2() {
     }
+    
     public Tea2(String name, int milk, boolean herbs, int sugar, int teaPowder) {
         this.name = name;
         this.milk = milk;
@@ -16,48 +17,62 @@ public class Tea2 {
         this.sugar = sugar;
         this.teaPowder = teaPowder;
     }
+    
     public Tea2(String name, int milk, boolean herbs, int sugar) {
         this(name, milk, herbs, sugar, DEFAULT_TEA_POWDER);
     }
     public Tea2(String name, int milk, boolean herbs) {
         this(name, milk, herbs, 0);
     }
+    
     public Tea2(String name, int milk) {
         this(name, milk, false);
     }
+    
     public Tea2(String name) {
         this(name, 0);
     }
+    
     public void setName(String name) {
         this.name = name;
     }
+    
     public String getName() {
         return name;
     }
+    
     public void setMilk(int milk) {
         this.milk = milk;
     }
+    
     public int getMilk() {
         return milk;
     }
+    
     public void setTeaPowder(int teaPowder) {
         this.teaPowder = teaPowder;
     }
+    
     public int getTeaPowder() {
         return teaPowder;
     }
+    
     public void setSugar(int sugar) {
         this.sugar = sugar;
     }
+    
     public int getSugar() {
         return sugar;
     }
+    
     public void setHerbs(boolean herbs) {
         this.herbs = herbs;
     }
+    
     public boolean getHerbs() {
         return herbs;
     }
+    
      public String toString() {
         return "Name: " +name +"\n"+"Milk:"+ milk+"\n"+"Herbs:"+herbs+"\n"+"Sugar:"+sugar+"\n"+"TeaPowder:"+teaPowder;
     }
