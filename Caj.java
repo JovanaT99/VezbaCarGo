52cc7a0851eb773e2352d95bc4b0a6b672f3cd9e
JovanaT99 <66609488+JovanaT99@users.noreply.github.com>
2020-10-12 17:18:59 +0200

    Update and rename Tea2.java to Caj.java

--- Caj.java
+++ Caj.java
@@ -1,4 +1,4 @@
-public class Tea2 {
+public class Caj {
     static final int DEFAULT_TEA_POWDER = 1;
 
     private String name;
@@ -7,10 +7,10 @@ public class Tea2 {
     private int sugar;
     private int teaPowder;
 
-    public Tea2() {
+    public Caj() {
     }
     
-    public Tea2(String name, int milk, boolean herbs, int sugar, int teaPowder) {
+    public Caj(String name, int milk, boolean herbs, int sugar, int teaPowder) {
         this.name = name;
         this.milk = milk;
         this.herbs = herbs;
@@ -18,18 +18,18 @@ public class Tea2 {
         this.teaPowder = teaPowder;
     }
     
-    public Tea2(String name, int milk, boolean herbs, int sugar) {
+    public Caj(String name, int milk, boolean herbs, int sugar) {
         this(name, milk, herbs, sugar, DEFAULT_TEA_POWDER);
     }
-    public Tea2(String name, int milk, boolean herbs) {
+    public Caj(String name, int milk, boolean herbs) {
         this(name, milk, herbs, 0);
     }
     
-    public Tea2(String name, int milk) {
+    public Caj(String name, int milk) {
         this(name, milk, false);
     }
     
-    public Tea2(String name) {
+    public Caj(String name) {
         this(name, 0);
     }
     
