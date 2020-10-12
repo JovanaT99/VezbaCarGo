2e19d0dbc100c9c84ecffd44abc311484f7ec5de
JovanaT99 <jovanablagojevic98@gmail.com>
2020-10-12 15:39:53 +0200

    Tea2

---
+++ Tea2.java
@@ -0,0 +1,61 @@
+public class Tea2 {
+    static final int DEFAULT_TEA_POWDER = 1;
+
+    private String name;
+    private int milk;
+    private boolean herbs;
+    private int sugar;
+    private int teaPowder;
+
+    public Tea2() {
+    }
+    public Tea2(String name, int milk, boolean herbs, int sugar, int teaPowder) {
+        this.name = name;
+        this.milk = milk;
+        this.herbs = herbs;
+        this.sugar = sugar;
+        this.teaPowder = teaPowder;
+    }
+    public Tea2(String name, int milk, boolean herbs, int sugar) {
+        this(name, milk, herbs, sugar, DEFAULT_TEA_POWDER);
+    }
+    public Tea2(String name, int milk, boolean herbs) {
+        this(name, milk, herbs, 0);
+    }
+    public Tea2(String name, int milk) {
+        this(name, milk, false);
+    }
+    public Tea2(String name) {
+        this(name, 0);
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setMilk(int milk) {
+        this.milk = milk;
+    }
+    public int getMilk() {
+        return milk;
+    }
+    public void setTeaPowder(int teaPowder) {
+        this.teaPowder = teaPowder;
+    }
+    public int getTeaPowder() {
+        return teaPowder;
+    }
+    public void setSugar(int sugar) {
+        this.sugar = sugar;
+    }
+    public int getSugar() {
+        return sugar;
+    }
+    public void setHerbs(boolean herbs) {
+        this.herbs = herbs;
+    }
+    public boolean getHerbs() {
+        return herbs;
+    }
+}
---
+++ TestD2.java
@@ -0,0 +1,7 @@
+public class TestD2 {
+    public static void main(String[] args) {
+
+        Tea2 tea2 = new Tea2("Black Tea");
+        System.out.println(tea2.DEFAULT_TEA_POWDER + "\n" + tea2.getName() + ',' + tea2.getMilk() + ',' + tea2.getSugar());
+    }
+}
