b19c52b983e3f5d56cb924b7d05b58090bb97b10
JovanaT99 <66609488+JovanaT99@users.noreply.github.com>
2020-10-12 12:23:18 +0200

    Update TestC.java

--- TestC.java
+++ TestC.java
@@ -1,9 +1,7 @@
 public class TestC {
 
     public static void main(String[] args) {
-
         Pas pas = new Pas("Ime psa je:"+ " "+"Dzeki","Rasa je:"+ ' '+"Bernadinac");
         System.out.println( pas.getIme()+ ',' + pas.getRasa());
     }
 }
-
