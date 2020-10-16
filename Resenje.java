9baecf7d5a7e58908b8b57351dbce60529ddb0b3
JovanaT99 <66609488+JovanaT99@users.noreply.github.com>
2020-10-16 16:20:12 +0200

    Update Resenje.java

--- Resenje.java
+++ Resenje.java
@@ -1,13 +1,9 @@
 class Resenje {
 
     public static void main(String args[]) {
-
         Adder adder = new Adder();
-
         Arthmetic arthmetic = new Arthmetic();
         System.out.println(arthmetic.getName());
         System.out.println(String.format("%d %d %d", arthmetic.add(12, 30), arthmetic.add(3, 10), arthmetic.add(10, 10)));
-
-
     }
 }
